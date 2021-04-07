bb551dfda5fd279cd68e0ba34c8e568439dc26f0
Avrumi Friedman <a.friedman07@gmail.com>
2021-04-07 00:34:15 -0400

    Add project files.

---
+++ QASite.Data/QASiteContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace QASite.Data
+{
+    public class QASiteContext : DbContext
+    {
+        private string _connectionString;
+
+        public QASiteContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //by default Entity Framework sets all foreign key relationship delete rules
+            //to be Cascade delete. This code changes it to be Restrict which is more in line
+            //of what we're used to in that it will fail deleting a parent, if there are still
+            //any children
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<QuestionsTags>()
+                .HasKey(qt => new { qt.QuestionId, qt.TagId });
+
+            modelBuilder.Entity<QuestionsTags>()
+                .HasOne(qt => qt.Question)
+                .WithMany(q => q.QuestionsTags)
+                .HasForeignKey(q => q.QuestionId);
+
+            modelBuilder.Entity<QuestionsTags>()
+                .HasOne(qt => qt.Tag)
+                .WithMany(t => t.QuestionsTags)
+                .HasForeignKey(q => q.TagId);
+
+            modelBuilder.Entity<QuestionLike>()
+                .HasKey(qt => new { qt.QuestionId, qt.UserId });
+
+            modelBuilder.Entity<QuestionLike>()
+                .HasOne(qt => qt.Question)
+                .WithMany(q => q.Likes)
+                .HasForeignKey(q => q.QuestionId);
+
+            modelBuilder.Entity<QuestionLike>()
+                .HasOne(qt => qt.User)
+                .WithMany(t => t.LikedQuestions)
+                .HasForeignKey(q => q.UserId);
+
+            
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<QuestionsTags> QuestionsTags { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<QuestionLike> QuestionLikes { get; set; }
+    }
+}
---
+++ QASite.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QASite.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using QASite.Data;
+
+namespace QASite.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _connectionString;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
+
+        public IActionResult Index()
+        {
+            var repo = new QuestionsRepository(_connectionString);
+            return View(repo.Get());
+        }
+
+    }
+}
